fd7de5469bb73be8f0d6ae69f01d9e72e9164fb0
Justin Perez <t-jperez@microsoft.com>
2022-08-09 09:29:46 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,60 @@
 use azure_kusto_data::prelude::KustoClient;
-use chrono::{DateTime, Duration, FixedOffset, SecondsFormat};
+use chrono::{DateTime, Duration, FixedOffset};
 use color_eyre::Result;
-use indicatif::{ProgressBar, ProgressIterator, ProgressStyle};
-use itertools::Itertools;
+use indicatif::{ProgressBar, ProgressStyle};
 use regex::Regex;
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware};
 use reqwest_retry::{policies::ExponentialBackoffBuilder, RetryTransientMiddleware};
+use reqwest_retry_after::RetryAfterMiddleware;
+use serde::Serialize;
 use std::{
-    collections::HashMap,
+    path::Path,
     sync::{
         atomic::{AtomicUsize, Ordering},
         Arc,
     },
 };
-use tokio::{select, sync::Mutex, time};
+use tokio::{
+    select,
+    sync::mpsc::{self, UnboundedSender},
+    time,
+};
 
-const KUSTO_CONN_STRING: &str = "server=https://engsrvprod.kusto.windows.net";
-const KUSTO_DATABASE: &str = "engineeringdata";
-const HELIX_LOGS_QUERY: &str = r#"TimelineRecords
-| where FinishTime > ago(7d)
-| where Result != "skipped"
-| where LogUri != ""
+const KUSTO_CONN_STRING: &str = "server=https://1es.kusto.windows.net;AAD Federated Security=True";
+const KUSTO_DATABASE: &str = "AzureDevOps";
+const HELIX_LOGS_QUERY: &str = r#"BuildTimelineRecord
+| where StartTime > ago(14d)
+| where OrganizationName == "dnceng"
 | where WorkerName startswith "NetCore1ESPool"
-| where Name has "helix"
-| join kind=inner (TimelineBuilds | project Definition, BuildId, Repository) on BuildId
+| where Type == "Task"
+| where isnotempty(LogId)
+| where RecordName has "Send" and RecordName has "Helix"
+| join kind = inner (Build
+    | project BuildStartTime = StartTime, BuildId, BuildEndTime = FinishTime, ProjectId)
+    on BuildId, ProjectId
+| project
+    OrganizationName,
+    ProjectId,
+    RepositoryId,
+    BuildId,
+    LogId,
+    HelixStart = StartTime,
+    BuildStartTime,
+    BuildEndTime
 "#;
 
-#[derive(Debug)]
+#[derive(Debug, Serialize)]
 struct OutputRecord {
-    wasted_time: i64,
-    start_time: DateTime<FixedOffset>,
-    worker_name: String,
-    definition_name: String,
-    repo: String,
+    repo_id: String,
+    build_start: String,
+    helix_test_start: String,
+    build_end: String,
+    wasted_time_sec: i64,
 }
+
 struct Config {
     client: ClientWithMiddleware,
     work_done: AtomicUsize,
-    output: Mutex<Vec<OutputRecord>>,
     send_job_regex: Regex,
     job_finished_regex: Regex,
     pat: &'static str,
@@ -46,26 +63,43 @@ struct Config {
 fn create_retry_client() -> ClientWithMiddleware {
     let retry_policy = ExponentialBackoffBuilder::default()
         .build_with_total_retry_duration(time::Duration::from_secs(10));
-    ClientBuilder::new(reqwest::Client::new())
-        .with(RetryTransientMiddleware::new_with_policy(retry_policy))
-        .build()
+    ClientBuilder::new(
+        reqwest::ClientBuilder::new()
+            .user_agent("helix-spinster")
+            .build()
+            .unwrap(),
+    )
+    .with(RetryAfterMiddleware::new())
+    .with(RetryTransientMiddleware::new_with_policy(retry_policy))
+    .build()
 }
 
-async fn worker_task(config: Arc<Config>, dt: Vec<serde_json::Value>) {
+async fn worker_task(
+    config: Arc<Config>,
+    dt: Vec<serde_json::Value>,
+    tx: UnboundedSender<OutputRecord>,
+) {
     // Get the LogUri
-    let log_uri = dt[18].as_str().unwrap();
+    let mut log_uri = String::with_capacity(128);
+    log_uri.push_str("https://dev.azure.com/");
+    log_uri.push_str(dt[0].as_str().unwrap());
+    log_uri.push('/');
+    log_uri.push_str(dt[1].as_str().unwrap());
+    log_uri.push_str("/_apis/build/builds/");
+    log_uri.push_str(&dt[3].as_u64().unwrap().to_string());
+    log_uri.push_str("/logs/");
+    log_uri.push_str(&dt[4].as_u64().unwrap().to_string());
 
-    if let Some(wait_time) = get_helix_wait_time(log_uri, &config).await {
+    if let Some(wait_time) = get_helix_wait_time(&log_uri, &config).await {
         let record = OutputRecord {
-            wasted_time: wait_time.num_seconds(),
-            start_time: DateTime::parse_from_rfc3339(dt[6].as_str().unwrap()).unwrap(),
-            worker_name: dt[12].as_str().unwrap().to_string(),
-            definition_name: dt[25].as_str().unwrap().to_string(),
-            repo: dt[27].as_str().unwrap().to_string(),
+            repo_id: dt[2].as_str().unwrap().to_string(),
+            build_start: dt[6].as_str().unwrap().to_string(),
+            helix_test_start: dt[5].as_str().unwrap().to_string(),
+            build_end: dt[7].as_str().unwrap().to_string(),
+            wasted_time_sec: wait_time.num_seconds(),
         };
 
-        let mut output = config.output.lock().await;
-        output.push(record);
+        tx.send(record).unwrap();
     }
 
     config.work_done.fetch_add(1, Ordering::Relaxed);
@@ -149,24 +183,26 @@ async fn main() -> Result<()> {
     let config = Arc::new(Config {
         client: create_retry_client(),
         work_done: AtomicUsize::new(0),
-        output: Mutex::new(Vec::with_capacity(100000)),
         send_job_regex: Regex::new(r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.[0-9]+Z)   Waiting for completion of job ([0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})").unwrap(),
         job_finished_regex: Regex::new(r"(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.[0-9]+Z)   Job ([0-9a-fA-F]{8}-(?:[0-9a-fA-F]{4}-){3}[0-9a-fA-F]{12})").unwrap(),
         pat: Box::leak(Box::new(std::env::args().nth(1).unwrap())),
     });
 
-    let mut items = 0usize;
+    let mut items = 0;
+    let (tx, mut rx) = mpsc::unbounded_channel();
+
     for r in result.primary_results() {
         for row in r.rows {
             let config = config.clone();
+            let tx = tx.clone();
             tokio::spawn(async move {
-                worker_task(config, row).await;
+                worker_task(config, row, tx).await;
             });
             items += 1;
         }
     }
 
-    let pb = ProgressBar::new(items as u64);
+    let pb = ProgressBar::new(items);
     pb.set_message("Processing helix logs");
     pb.set_style(
         ProgressStyle::with_template(
@@ -175,39 +211,13 @@ async fn main() -> Result<()> {
         .unwrap(),
     );
 
-    while items != config.work_done.load(Ordering::Relaxed) {
-        time::sleep(time::Duration::from_millis(100)).await;
-        pb.set_position(config.work_done.load(Ordering::Relaxed) as u64)
+    let mut writer = csv::Writer::from_path(Path::new("helix_logs.csv")).unwrap();
+    while let Some(record) = rx.recv().await {
+        pb.inc(1);
+        writer.serialize(record).unwrap();
     }
 
     pb.finish_with_message("Done parsing helix logs :)");
 
-    let lock = config.output.lock().await;
-    export_csv(&lock, "helix_logs.csv");
-
-    println!("good bye");
-
     Ok(())
-}
-
-fn export_csv(data: &[OutputRecord], filename: &str) {
-    let mut wtr = csv::Writer::from_path(filename).unwrap();
-    wtr.write_record(&[
-        "start_time",
-        "worker_name",
-        "wasted_time",
-        "definition",
-        "repository",
-    ])
-    .unwrap();
-    for record in data.iter().progress().with_message("writing to csv") {
-        wtr.write_record(&[
-            &record.start_time.to_rfc3339_opts(SecondsFormat::Secs, true),
-            &record.worker_name,
-            &record.wasted_time.to_string(),
-            &record.definition_name,
-            &record.repo,
-        ])
-        .unwrap();
-    }
 }
